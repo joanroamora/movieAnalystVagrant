ff61fe49f1f86330289d3e31e84457998121b191
JOAN ROA <joanroamora@gmail.com>
2022-08-05 20:59:10 -0500

    Setting first commands on bash script.

--- data/augus.sh
+++ data/augus.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
+echo "Setting the Environment Variables ***********************************************************************"
+export BACK_HOST="127.0.0.1"
+export DB_HOST="moviedb-rds.c5wjzkwslaaz.sa-east-1.rds.amazonaws.com"
+export DB_USER="applicationuser"
+export DB_PASS="Colombia2021*"
+export DB_NAME="movie_db"
+#echo $BACK_HOST $DB_HOST $DB_NAME $DB_PASS $DB_USER
 
+echo "Updating and Upgrading your System **********************************************************************"
 sudo apt-get update -y
 sudo apt upgrade -y
 
+echo "Software Installation in progress ***********************************************************************"
 sudo apt install -y nodejs
 sudo apt install -y npm
 
-cd ./data/movie-analyst-ui
-sudo npm install 
-node serverjs
+echo "Cloning repositories and directory optimization *********************************************************"
+cd /home/vagrant/data/movie-analyst-ui
+echo pwd
+
+
+#echo "cd home/vagrant/data/movie-analyst-ui XXXXXXXXXXXXXXXXXX"
+#sudo npm install 
+#echo "npm install XXXXXXXXXXXXXXXXXX"
+#node server.js
