6c3162124282463f427b887be8221251c534ca1f
JOAN ROA <joanroamora@gmail.com>
2022-08-06 15:20:58 -0500

    Setup Network vagrant implementation

---
+++ scriptTestUI.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#sudo -u vagrant git clone https://github.com/jeisonroa1/movie-analyst-$(hostname).git
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install node
+sudo apt-get -y install npm
+sudo npm cache clean -f
+npm install -g n
+sudo n stable
+#cd ./movie-analyst-$(hostname)/
+cd /home/vagrant/data/movie-analyst-ui
+#sudo -u vagrant git checkout nosql
+#sudo ln -s /usr/bin/nodejs /usr/bin/node
+sudo npm install
+#nohup node server.js &
+node server.js
